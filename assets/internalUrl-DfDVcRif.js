6b554c50b0ff177fc8d4f27dd5fd643f88f19965
beherw <beherw@users.noreply.github.com>
2026-01-23 15:13:25 +0000

    deploy: b993afdc4eee5483625fa49a5a27b55dc9ce49c7

---
+++ assets/internalUrl-DfDVcRif.js
@@ -0,0 +1,1 @@
+function r(e){return`/FFXIV_Market/${e}`.replace(/\/+/g,"/")}export{r as g};
--- assets/internalUrl-RmprVjPU.js
+++
@@ -1,1 +0,0 @@
-function s(t){return`${window.location.hostname==="beherw.github.io"?"/FFXIV_Market":""}${t}`}export{s as g};
